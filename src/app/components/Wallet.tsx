d6ac17771ad7ada9a40e1297267531165b91a643
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-09-20 23:00:37 +0800

    chore: add airdao networks

--- src/app/components/Wallet.tsx
+++ src/app/components/Wallet.tsx
@@ -6,6 +6,38 @@ import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 // Setting up list of evmNetworks
 const evmNetworks = [
+    // AirDAO testnet
+    {
+        blockExplorerUrls: ["https://testnet.airdao.io/explorer/"],
+        chainId: 22040,
+        chainName: "AirDAO Testnet",
+        iconUrls: ["airdao-logo.svg"],
+        name: "AirDAO",
+        nativeCurrency: {
+            decimals: 18,
+            name: "Ambrosus",
+            symbol: "AMB",
+        },
+        networkId: 22040,
+        rpcUrls: ["https://network.ambrosus-test.io"],
+        vanityName: "AirDAO Testnet",
+    },
+    // AirDAO mainnet
+    {
+        blockExplorerUrls: ["https://airdao.io/explorer/"],
+        chainId: 16718,
+        chainName: "AirDAO Mainnet",
+        iconUrls: ["airdao-logo.svg"],
+        name: "AirDAO",
+        nativeCurrency: {
+            decimals: 18,
+            name: "Ambrosus",
+            symbol: "AMB",
+        },
+        networkId: 16718,
+        rpcUrls: ["https://network.ambrosus.io/"],
+        vanityName: "AirDAO Mainnet",
+    },
     {
         blockExplorerUrls: ["https://etherscan.io/"],
         chainId: 1,
