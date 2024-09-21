79c66a59bbd18dee0b86a4ad15b3177fe1d4b588
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-09-21 22:44:10 +0800

    chore: switch networks

--- src/app/components/TelegramWallet.tsx
+++ src/app/components/TelegramWallet.tsx
@@ -2,41 +2,79 @@
 import { createWalletClient, http, parseEther, Chain } from "viem";
 import { english, generateMnemonic, mnemonicToAccount } from "viem/accounts";
 import { mainnet, sepolia, goerli } from "viem/chains";
+import { defineChain } from "viem/utils";
 import { FormEventHandler, useEffect, useState } from "react";
 import WebApp from "@twa-dev/sdk";
+
+const AirDAOTestnet = defineChain({
+    id: 22040,
+    name: "AirDAO testnet",
+    nativeCurrency: { name: "Ambrosus", symbol: "AMB", decimals: 18 },
+    rpcUrls: { default: { http: ["https://network.ambrosus-test.io"] } },
+    blockExplorers: {
+        default: {
+            name: "AirDAO Explorer",
+            url: "https://testnet.airdao.io/explorer",
+            apiUrl: "https://api.etherscan.io/api",
+        },
+    },
+    contracts: {
+        ensRegistry: {
+            address:
+                "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e" as `0x${string}`,
+        },
+        ensUniversalResolver: {
+            address:
+                "0xce01f8eee7E479C928F8919abD53E553a36CeF67" as `0x${string}`,
+            blockCreated: 19_258_213,
+        },
+        multicall3: {
+            address:
+                "0xca11bde05977b3631167028862be2a173976ca11" as `0x${string}`,
+            blockCreated: 14_353_601,
+        },
+    },
+});
+
+const AirDAO = defineChain({
+    id: 16718,
+    name: "AirDAO mainnet",
+    nativeCurrency: { name: "Ambrosus", symbol: "AMB", decimals: 18 },
+    rpcUrls: { default: { http: ["https://network.ambrosus.io/"] } },
+    blockExplorers: {
+        default: {
+            name: "AirDAO Explorer",
+            url: "https://airdao.io/explorer",
+            apiUrl: "https://api.etherscan.io/api",
+        },
+    },
+    contracts: {
+        ensRegistry: {
+            address:
+                "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e" as `0x${string}`,
+        },
+        ensUniversalResolver: {
+            address:
+                "0xce01f8eee7E479C928F8919abD53E553a36CeF67" as `0x${string}`,
+            blockCreated: 19_258_213,
+        },
+        multicall3: {
+            address:
+                "0xca11bde05977b3631167028862be2a173976ca11" as `0x${string}`,
+            blockCreated: 14_353_601,
+        },
+    },
+});
 
 const networks: { [key: string]: Chain } = {
     Mainnet: mainnet,
     Sepolia: sepolia,
-    AirDAOTestnet: {
-        id: 22040,
-        name: "AirDAO testnet",
-        nativeCurrency: { name: "Ambrosus", symbol: "AMB", decimals: 18 },
-        rpcUrls: { default: { http: ["https://network.ambrosus-test.io"] } },
-        blockExplorers: {
-            default: {
-                name: "AirDAO Explorer",
-                url: "https://testnet.airdao.io/explorer",
-                apiUrl: "https://api.etherscan.io/api",
-            },
-        },
-    },
-    AirDAO: {
-        id: 16718,
-        name: "AirDAO mainnet",
-        nativeCurrency: { name: "Ambrosus", symbol: "AMB", decimals: 18 },
-        rpcUrls: { default: { http: ["https://network.ambrosus.io/"] } },
-        blockExplorers: {
-            default: {
-                name: "AirDAO Explorer",
-                url: "https://airdao.io/explorer",
-                apiUrl: "https://api.etherscan.io/api",
-            },
-        },
-    },
+    AirDAOTestnet: AirDAOTestnet,
+    AirDAO: AirDAO,
 };
 
 export default function TelegramWallet() {
+    const [txnHash, setTxnHash] = useState("");
     const [address, setAddress] = useState("");
     const [message, setMessage] = useState("");
     const [balance, setBalance] = useState(0);
@@ -44,17 +82,13 @@ export default function TelegramWallet() {
     const [copySuccess, setCopySuccess] = useState("");
     const [showMnemonicText, setShowMnemonicText] = useState(false);
     const [mnemonicCopySuccess, setMnemonicCopySuccess] = useState("");
-    const [selectedNetwork, setSelectedNetwork] = useState<Chain>(mainnet);
+    const [selectedNetwork, setSelectedNetwork] = useState<any>(mainnet);
 
     function getAccount() {
-        const client = createWalletClient({
-            chain: selectedNetwork,
-            transport: http(),
-        });
         WebApp.CloudStorage.getItem("mnemonic", (error, result) => {
             if (error) {
                 setMessage(JSON.stringify(error));
-                
+
                 return null;
             }
             if (result) {
@@ -116,19 +150,34 @@ export default function TelegramWallet() {
         const address = formData.get("address") as string;
         const amount = formData.get("amount") as string;
 
-        const account = getAccount();
+        WebApp.CloudStorage.getItem("mnemonic", async (error, result) => {
+            if (error) {
+                setMessage(JSON.stringify(error));
 
-        const client = createWalletClient({
-            chain: selectedNetwork,
-            transport: http(),
-        });
+                return null;
+            }
+            let account = undefined;
+            if (result) {
+                account = mnemonicToAccount(result);
+            } else {
+                const mnemonic = generateMnemonic(english);
+                account = mnemonicToAccount(mnemonic);
+                WebApp.CloudStorage.setItem("mnemonic", mnemonic);
+                setAddress(account.address);
+            }
+            const client = createWalletClient({
+                chain: selectedNetwork,
+                transport: http(),
+            });
 
-        const hash = await client.sendTransaction({
-            account: account as any,
-            to: address as `0x${string}`,
-            value: amount ? parseEther(amount) : undefined,
+            const hash = await client.sendTransaction({
+                account: account as any,
+                to: address as `0x${string}`,
+                value: amount ? parseEther(amount) : undefined,
+                chain: selectedNetwork,
+            });
+            setTxnHash(hash);
         });
-        // setTxnHash(hash);
     };
 
     function toggleMnemonic() {
@@ -154,7 +203,7 @@ export default function TelegramWallet() {
         event: React.ChangeEvent<HTMLSelectElement>
     ) => {
         const networkName = event.target.value;
-        setSelectedNetwork(networks[networkName]);
+        setSelectedNetwork(networks[networkName] as any);
     };
 
     return (
@@ -175,13 +224,15 @@ export default function TelegramWallet() {
                     )}
                 </div>
                 <select
-                    value={selectedNetwork.name}
+                    value={Object.keys(networks).find(
+                        (key) => networks[key].id === selectedNetwork.id
+                    )}
                     onChange={handleNetworkChange}
                     className="px-2 py-1 bg-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-gray-400"
                 >
                     {Object.keys(networks).map((networkName) => (
                         <option key={networkName} value={networkName}>
-                            {networkName}
+                            {networks[networkName].name}
                         </option>
                     ))}
                 </select>
@@ -230,7 +281,7 @@ export default function TelegramWallet() {
                 <input name="amount" type="text" required placeholder="0.05" />
                 <button type="submit">Send</button>
                 <span data-testid="transaction-section-result-hash">
-                    {txnHash}
+                    {txnHash !== "" && `Tx Hash: ${formatAddress(txnHash)}`}
                 </span>
             </form> */}
         </div>
